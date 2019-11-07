52c6369680175c9b9d28d526752c33f656053b86
sunkast <sunkast@users.noreply.github.com>
2019-11-06 19:27:11 -0500

    Update MMM-Favicon.js

--- MMM-Favicon.js
+++ MMM-Favicon.js
@@ -2,6 +2,9 @@ Module.register("MMM-Favicon", {
 		defaults: {
 		icon: 'icon.png'
 	},
+	getStyles: function() {
+		return ["MMM-Favicon.css"];
+	},
 getDom: function() {
 var icon = document.querySelector('link[rel="icon"]').setAttribute('href', this.file(this.config.icon));
 
